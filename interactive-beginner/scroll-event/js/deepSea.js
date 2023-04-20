12d9fb0bcfe51292311918310cdb5c346dcebe36
jaiyah-sg <es-jaiyah@smilegate.com>
2023-04-20 14:53:44 +0900

    deep dive done

--- interactive-beginner/scroll-event/js/deepSea.js
+++ interactive-beginner/scroll-event/js/deepSea.js
@@ -7,10 +7,16 @@ let scrollNum = 0;
 let documentHeight = 0;
 let per = 0;
 
-console.log(submarine);
+let widthValueByScroll = 0;
 
+// console.log(submarine);
+
+// body 의 scrollHeight, offsetHeight 는 같음
+// console.log( document.body.scrollHeight, document.body.offsetHeight);
 window.addEventListener("scroll", () => {
     scrollNum = window.scrollY;
+
+    // 실제 스크롤만 되는 높이
     documentHeight = document.body.scrollHeight - window.innerHeight;
 
     per = percent(scrollNum, documentHeight);
@@ -18,8 +24,16 @@ window.addEventListener("scroll", () => {
     depthWrap.querySelector("span").innerText = scrollNum.toFixed(0);
     progressBar.style.width = per + "%";
 
-    submarine.style.transform = `translateX(${per}%)`;
+    // submarine.style.transform = `translateX(${per}%)`;
+    widthValueByScroll = (scrollNum) / documentHeight * innerWidth;
+    console.log(widthValueByScroll - 80)
+    if(widthValueByScroll >= innerWidth - 100) { 
+        widthValueByScroll = innerWidth - 100;
+    }
+    submarine.style.transform = `translateX(${ widthValueByScroll -= 50 }px)`;
     octopus.style.transform = `translateY(${-per / 3}%)`;
+
+    // console.log( scrollNum / documentHeight * innerWidth );
 });
 
 const percent = (num, totalNum) => {
